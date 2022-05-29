f450a519b740218636594b1340f3823a548ab6f3
Violeta Malinova <violeta@mlnova.com>
2022-05-29 14:13:27 +0300

    enable internet and data binding

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchButton: Button
+    private lateinit var historyButton: Button
+    private lateinit var searchEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+
+        searchButton = binding.searchButton
+        historyButton = binding.searchHistoryButton
+        searchEditText = binding.editTextCity;
+
+        searchButton.setOnClickListener {
+            Toast.makeText(applicationContext, searchEditText.text, Toast.LENGTH_LONG).show()
+        }
     }
 }
