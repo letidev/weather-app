3babdf091256fc552f58087583d6e55d3660272b
Violeta Malinova <violeta@mlnova.com>
2022-06-04 15:50:42 +0300

    adjust layout

--- app/src/main/java/com/example/weatherapp/weatherdetail/WeatherDetailsViewModel.kt
+++ app/src/main/java/com/example/weatherapp/weatherdetail/WeatherDetailsViewModel.kt
@@ -13,16 +13,13 @@ import java.lang.Exception
 
 private const val units = "metric";
 private const val appid = "78c51d4bee44b9fe91dfc096673678ef"
-enum class WeatherApiStatus { LOADING, ERROR, DONE, NOT_FOUND }
 
 class WeatherDetailsViewModel : ViewModel() {
     private val _resultLabel = MutableLiveData<String>();
     private val _list = MutableLiveData<List<WeatherListItem>>();
-    private val _status = MutableLiveData<WeatherApiStatus>()
 
     val resultLabel: LiveData<String> = _resultLabel;
     val list: LiveData<List<WeatherListItem>> = _list
-    val status: LiveData<WeatherApiStatus> = _status
 
     init {
         _resultLabel.value = "Loading..."
@@ -30,21 +27,17 @@ class WeatherDetailsViewModel : ViewModel() {
 
     fun getCurrentWeather(city: String) {
         viewModelScope.launch {
-            _status.value = WeatherApiStatus.LOADING
             try {
                 val response = WeatherApi.retrofitService.getCurrentWeather(city, units, appid)
-                _status.value = WeatherApiStatus.DONE
                 _resultLabel.value = "Showing weather for ${response.city.name}, ${response.city.country}";
                 _list.value = response.list;
             } catch(e: HttpException) {
                 if(e.code() == 404) {
                     _resultLabel.value = "City not found";
-                    _status.value = WeatherApiStatus.NOT_FOUND
                 } else {
                     throw Exception(e)
                 }
             } catch (e: Exception) {
-                _status.value = WeatherApiStatus.ERROR
                 _resultLabel.value = e.message
             }
         }
