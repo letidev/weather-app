e63fb4a231bc3552733856da60d2f9aba21ec531
Violeta Malinova <violeta@mlnova.com>
2022-06-06 17:33:39 +0300

    show weather icon

--- app/src/main/java/com/example/weatherapp/BindingAdapters.kt
+++ app/src/main/java/com/example/weatherapp/BindingAdapters.kt
@@ -1,9 +1,13 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.weatherapp.history.HistoryListAdapter
 import com.example.weatherapp.history.SearchHistoryViewModel
 import com.example.weatherapp.history.SearchItem
@@ -12,17 +16,17 @@ import com.example.weatherapp.weatherdetail.WeatherListAdapter
 
 @BindingAdapter("dataTemp")
 fun bindTemperature(textView: TextView, dataTemp: Double?) {
-    textView.text = "Temperature: ${dataTemp.toString()}";
+    textView.text = "Temperature: ${dataTemp.toString()}°";
 }
 
 @BindingAdapter("dataMinTemp")
 fun bindMinTemperature(textView: TextView, dataTemp: Double?) {
-    textView.text = "Min temperature: ${dataTemp.toString()}";
+    textView.text = "Min temperature: ${dataTemp.toString()}°";
 }
 
 @BindingAdapter("dataMaxTemp")
 fun bindMaxTemperature(textView: TextView, dataTemp: Double?) {
-    textView.text = "Max temperature ${dataTemp.toString()}";
+    textView.text = "Max temperature ${dataTemp.toString()}°";
 }
 
 @BindingAdapter("weatherListData")
@@ -42,6 +46,20 @@ fun bindHistoryRecyclerView(button: Button, data: Int?, viewModel: SearchHistory
     button.setOnClickListener {
         val db = DBHelper(button.context)
         db.deleteSearch(data)
-        viewModel?.getSearches(button.context)
+        val intent = Intent(button.context, MainActivity::class.java)
+        button.context.startActivity(intent);
+    }
+}
+
+@BindingAdapter("iconId")
+fun bindImageView(imageView: ImageView, iconId: String?) {
+    iconId?.let {
+        val imgUrl = "http://openweathermap.org/img/wn/$iconId@2x.png"
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+
+        imageView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
     }
 }
